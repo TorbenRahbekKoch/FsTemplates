b54e22bf4a5631ae5980a49afbda3fa595792d5a
Torben Rahbek Koch <torben@rahbekkoch.dk>
2014-04-23 23:46:18 +0200

    Added case-insensivity to header lookups

--- Projects/Routing/RequestExtensions.fs
+++ Projects/Routing/RequestExtensions.fs
@@ -1,15 +1,40 @@
 ﻿namespace Routing
 [<AutoOpen>]
 module RequestExtensions =
+    open System.Collections.Generic
+    open System.Globalization
     open System.Linq
     open Microsoft.Owin
+    
+    [<Literal>]
+    let ``Upper case header key`` = "FsTemplates.UpperCaseHeaders"
 
     type IOwinRequest with
-    member this.header headerName =
-        this.Headers.[headerName]
-    member this.hasHeader headerName =
-        this.Headers.ContainsKey headerName
-    member this.hasHeaderWith headerName headerValue =
-        let found, value = this.Headers.TryGetValue headerName
-        found && value.Any(fun v -> v.Contains(headerValue))
+    
+    member this.UpperCaseHeaders() =
+        let found, cached = this.Environment.TryGetValue(``Upper case header key``)
+        if found then
+            cached :?> IDictionary<string, string[]>
+        else
+            let t = this.Headers    
+                    |> Seq.map(fun header -> 
+                        (header.Key.ToUpper CultureInfo.InvariantCulture, 
+                         header.Value |> Array.map(fun value -> value.ToUpper CultureInfo.InvariantCulture)))
+                    |> dict
+            this.Environment.[``Upper case header key``] <- t
+            t
+
+    member this.header (headerName: string) =
+        let upperCaseHeaderName = headerName.ToUpper(CultureInfo.InvariantCulture)
+        this.UpperCaseHeaders().[upperCaseHeaderName]
+
+    member this.hasHeader (headerName: string) =
+        let upperCaseHeaderName = headerName.ToUpper(CultureInfo.InvariantCulture)
+        this.UpperCaseHeaders().ContainsKey upperCaseHeaderName
+
+    member this.hasHeaderWith (headerName: string) (headerValue: string) =
+        let upperCaseHeaderName = headerName.ToUpper(CultureInfo.InvariantCulture)
+        let upperCaseHeaderValue = headerValue.ToUpper(CultureInfo.InvariantCulture)
+        let found, value = this.UpperCaseHeaders().TryGetValue upperCaseHeaderName
+        found && value.Any(fun v -> v.Contains(upperCaseHeaderValue))
 
